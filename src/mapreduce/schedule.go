a832ae1cd87a9db687138877dfe1f1f59a786123
ray wang <wangrui@bongmi.com>
2017-11-01 19:14:45 +0800

    lab 1_3

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
@@ -22,7 +25,36 @@ func (mr *Master) schedule(phase jobPhase) {
 	// Remember that workers may fail, and that any given worker may finish
 	// multiple tasks.
 	//
-	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-	//
+
+	for i := 0; i < ntasks; i++ {
+		fn := ""
+		if phase == mapPhase {
+			fn = mr.files[i]
+		}
+		dtas := DoTaskArgs{
+			JobName:       mr.jobName,
+			File:          fn,
+			Phase:         phase,
+			TaskNumber:    i,
+			NumOtherPhase: nios,
+		}
+		w := <-mr.registerChannel
+		debug("To Task for %s NO%d", w, i)
+		go runAndRestartWorker(w, mr.address, dtas)
+	}
 	fmt.Printf("Schedule: %v phase done\n", phase)
+}
+
+// assuming call always return true
+func runAndRestartWorker(w, master string, dargs DoTaskArgs) {
+	suc := call(w, "Worker.DoTask", dargs, new(struct{}))
+	if !suc {
+		log.Fatalf("Call %s Dotask failed", w)
+	}
+	rargs := new(RegisterArgs)
+	rargs.Worker = w
+	ok := call(master, "Master.Register", rargs, new(struct{}))
+	if ok == false {
+		fmt.Printf("Register: RPC %s register error\n", master)
+	}
 }
