8ee393894ff5606750d43d35047e321e5b95f28d
ray wang <wangrui@bongmi.com>
2017-11-01 22:18:53 +0800

    lab 1_4

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -2,7 +2,6 @@ package mapreduce
 
 import (
 	"fmt"
-	"log"
 )
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
@@ -31,30 +30,36 @@ func (mr *Master) schedule(phase jobPhase) {
 		if phase == mapPhase {
 			fn = mr.files[i]
 		}
-		dtas := DoTaskArgs{
+		dtas := &DoTaskArgs{
 			JobName:       mr.jobName,
 			File:          fn,
 			Phase:         phase,
 			TaskNumber:    i,
 			NumOtherPhase: nios,
 		}
-		w := <-mr.registerChannel
-		debug("To Task for %s NO%d", w, i)
-		go runAndRestartWorker(w, mr.address, dtas)
+		waitToDespatchOneTask(mr, i, dtas)
 	}
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
 
 // assuming call always return true
-func runAndRestartWorker(w, master string, dargs DoTaskArgs) {
+func runAndRestartWorker(w string, m *Master, taskId int, dargs *DoTaskArgs) {
 	suc := call(w, "Worker.DoTask", dargs, new(struct{}))
 	if !suc {
-		log.Fatalf("Call %s Dotask failed", w)
+		fmt.Printf("Call %s Dotask failed", w)
+		waitToDespatchOneTask(m, taskId, dargs)
+		return
 	}
 	rargs := new(RegisterArgs)
 	rargs.Worker = w
-	ok := call(master, "Master.Register", rargs, new(struct{}))
+	ok := call(m.address, "Master.Register", rargs, new(struct{}))
 	if ok == false {
-		fmt.Printf("Register: RPC %s register error\n", master)
+		fmt.Printf("Register: RPC %s register error\n", m.address)
 	}
+}
+
+func waitToDespatchOneTask(m *Master, taskId int, dtas *DoTaskArgs) {
+	w := <-m.registerChannel
+	debug("To Task for %s NO%d", w, taskId)
+	go runAndRestartWorker(w, m, taskId, dtas)
 }
