5b8a4a37e99b0a6008b95dff8ddab6f6c84bee50
ray wang <wangrui@bongmi.com>
2017-11-08 22:49:11 +0800

    rewrite receive heart beat logic

---
+++ src/raft/user_recvHeartBeat.go
@@ -0,0 +1,36 @@
+package raft
+
+import "time"
+
+// for follower to start receiving (want) heart beats
+func (rf *Raft) startRecvHeartBeats() {
+	rf.SetUserState(InRecvHeartBeat)
+	canceled := false
+
+	timer := time.NewTimer(randomTimeOut())
+
+	for !canceled {
+		select {
+		case <-timer.C:
+			//DPrintf("HeartBeat Time Out msg %d %v\n", rf.me, time.Now())
+			rf.SetUserState(None)
+			rf.becomeCandidate()
+			canceled = true
+			timer.Stop()
+		case <-rf.abort:
+			//DPrintf("HB recv abort msg %d\n", rf.me)
+			rf.SetUserState(None)
+			canceled = true
+			timer.Stop()
+		case <-rf.heartBeart:
+			// reset timer
+			if !timer.Stop() {
+				select {
+				case <-timer.C:
+				default:
+				}
+			}
+			timer.Reset(randomTimeOut())
+		}
+	}
+}
