d1ebfab9b31c0019841c44717071df3854dd6644
ray wang <wangrui@bongmi.com>
2017-11-11 21:00:12 +0800

    update

--- src/raft/util.go
+++ src/raft/util.go
@@ -11,3 +11,30 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	}
 	return
 }
+
+const VoteDebugFlag = true
+
+func DVotePrintf(fmt string, a ...interface{}) (n int, err error) {
+	if VoteDebugFlag {
+		return DPrintf(fmt, a...)
+	}
+	return
+}
+
+const HeartBeatDebugFlag = false
+
+func DHBPrintf(fmt string, a ...interface{}) (n int, err error) {
+	if HeartBeatDebugFlag {
+		return DPrintf(fmt, a...)
+	}
+	return
+}
+
+const LogDebugFlag = false
+
+func DLogPrintf(fmt string, a ...interface{}) (n int, err error) {
+	if LogDebugFlag {
+		return DPrintf(fmt, a...)
+	}
+	return
+}
