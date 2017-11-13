d039e17a5b4c80730a5b0916f02581086a6ec22c
ray wang <wangrui@bongmi.com>
2017-11-13 19:28:58 +0800

    recode append logs, add logic for sync logs from leader to others, and sync apply msg for server

--- src/raft/user_appendLog.go
+++ src/raft/user_appendLog.go
@@ -1,6 +1,8 @@
 package raft
 
-import "sync"
+import (
+	"sync"
+)
 
 // TODO
 var argId = 0
@@ -15,45 +17,46 @@ func getId() int {
 	return res
 }
 
-func (rf *Raft) appendLogToOthers() {
-	log := rf.logs[len(rf.logs)-1]
+// role: leader
+// broadcast AE RPC to all followers, in order to copy logs to them
+// once the majority says YES
+// leader update its commit index to the last
+
+func (rf *Raft) syncLogsToOthers() {
 	wg := &sync.WaitGroup{}
 	replyCh := make(chan *AppendEntriesReply)
-
+	DLogPrintf("Sender %d Phase 1 SyncLogs %v\n", rf.me, rf.logs)
 	for i := range rf.peers {
 		if i == rf.me {
 			continue
 		}
-		rf.nextIndex[i] = len(rf.logs) - 1
+		rf.nextIndex[i] = len(rf.logs) - 1 // ? every time ?
 		go rf.sendAppendLogsTo(i, replyCh, wg)
 	}
-	DLogPrintf("Phase 1\n")
 
-	foundHigherT := false
 	suc := 1
-
-	done := uint(1 << uint(rf.me)) // bit map for recording succ
-
+	//done := uint(1 << uint(rf.me)) // bit map for recording succ
+	foundHigherT := false
 	for !rf.MostAgreed(suc) {
-		//DLogPrintf("next idxs: %v\n", rf.nextIndex)
 		rpl := <-replyCh
 		if rpl.Success {
 			DLogPrintf("Succ %d \n", rpl.Me)
-			done |= 1 << uint(rpl.Me)
+			//done |= 1 << uint(rpl.Me)
+			// update matches
 			rf.matchIndex[rpl.Me] = len(rf.logs) - 1
 			suc++
+		} else if rpl.Term > rpl.Req.Term {
+			DLogPrintf("Found Higher Term in Reply %d > %d for ae Send %d Recv %d\n",
+				rpl.Term, rf.currentTerm, rf.me, rpl.Me)
+			foundHigherT = true
 		} else {
-			if rpl.Term > rf.currentTerm {
-				DLogPrintf("Found Higher Term in Reply %d > %d\n", rpl.Term, rf.currentTerm)
-				foundHigherT = true
-				break // break loop
-			} else {
-				to := rpl.Me
-				rf.nextIndex[to]--
-				//DLogPrintf("-- %d, now is %d\n", rpl.Me, rf.nextIndex[to])
-				wg.Add(1)
-				go rf.sendAppendLogsTo(to, replyCh, wg) // right to send / receive msgs through replyCh, for want result
-			}
+			to := rpl.Me
+			rf.mu.Lock()
+			rf.nextIndex[to]--
+			rf.mu.Unlock()
+			//DLogPrintf("-- %d, now is %d\n", rpl.Me, rf.nextIndex[to])
+			wg.Add(1)
+			go rf.sendAppendLogsTo(to, replyCh, wg) // right to send / receive msgs through replyCh, for want result
 		}
 	}
 
@@ -61,23 +64,19 @@ func (rf *Raft) appendLogToOthers() {
 		// finding a higher term
 		// notify all,
 		go dranRplChA(replyCh, rf.me, rf.currentTerm)
+		rf.abort <- struct{}{}
 		rf.becomeFollower()
 	} else {
-		// agreed for most server
-		// TODO, just increase index ?
-		msg := &ApplyMsg{Index: len(rf.logs), Command: log.Content}
-
-		rf.applyCh <- *msg
-		rf.lastApplied ++
-		DLogPrintf("Committed! commit index %d matches nexts:%v %v done:%v\n",
-			rf.commitIndex, rf.matchIndex, rf.nextIndex, decodeBitMap(done))
-		// TODO ,insert log to see done bitmap and
-		// send committed rpcs to all followers
+		// Success routine::
+		// agreed for most server, send AE to rest of them in another goroutine
+		DLogPrintf("Most Agreed \n")
+		rf.commitIndex = len(rf.logs)
 		for i := range rf.peers {
-			if (done & (1 << uint(i))) > 0 {
+			//if (done & (1 << uint(i))) > 0 {
+			if i == rf.me {
 				continue
 			}
-			//DLogPrintf("Resend commit ae indx %d to %d\n", rf.commitIndex, i)
+			DLogPrintf("Resend commit ae index %d to %d\n", rf.commitIndex, i)
 			go func(rf *Raft, i int) {
 				notified := false
 				for rf.IsLeader() && !notified {
@@ -86,18 +85,21 @@ func (rf *Raft) appendLogToOthers() {
 					ok := rf.sendAppendEntries(i, *args, reply)
 					if !ok {
 						DPrintf("send AppendEntries to %d failed\n", i)
+						return
 					}
 					if reply.Success {
-						DLogPrintf("Id %d Committed for %d\n", reply.Id, reply.Me)
+						DLogPrintf("Id %d Committed for %d\n", reply.Req.Id, reply.Me)
 						notified = true
 					} else {
-						rf.mu.Lock()
-						t := rf.currentTerm
-						rf.mu.Unlock()
-						if t >= reply.Term {
+						if reply.Req.Term >= reply.Term {
+							rf.mu.Lock()
 							rf.nextIndex[i]--
+							rf.mu.Unlock()
 							// resend
 						} else {
+							DLogPrintf("Id %d TODO recv %d Asking Term %d < Reply Term %d\n",
+								reply.Req.Id, reply.Me, reply.Req.Term, reply.Term)
+							// asking Term < replying Term
 							// should break? or leave it
 						}
 					}
@@ -132,4 +134,24 @@ func (rf *Raft) sendAppendLogsTo(
 		//DPrintf("send AppendEntries to %d failed\n", server)
 	}
 	replyCh <- reply
+}
+
+// apply msg til rf's apply index reaches the commit index
+func (rf *Raft) syncApplyMsgs() {
+	for rf.lastApplied < rf.commitIndex {
+		rf.lastApplied++
+		rf.mu.Lock()
+		ind := rf.lastApplied
+		rf.mu.Unlock()
+		if ind <= 0 {
+			continue
+		}
+
+		msg := ApplyMsg{
+			Index:   ind,
+			Command: rf.logs[ind-1].Content,
+		}
+		rf.applyCh <- msg
+		DLogPrintf("Done Apply %v ind %d [APPLYEE] %d\n", msg.Command, msg.Index, rf.me)
+	}
 }
