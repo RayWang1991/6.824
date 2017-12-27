6277f72eebc9bcd16250dafc5126cb999be9fddd
ray wang <wangrui@bongmi.com>
2017-12-27 23:07:45 +0800

    todo, find and fix the time out bug

--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -5,6 +5,11 @@ import (
 	"6.824/src/raft"
 	"encoding/gob"
 	"sync"
+	"bytes"
+	"fmt"
+	"strconv"
+	"time"
+	"strings"
 )
 
 type Op struct {
@@ -18,18 +23,115 @@ type RaftKV struct {
 	me      int
 	rf      *raft.Raft
 	applyCh chan raft.ApplyMsg
+	syncCh  chan string // sync apply disposer and front end
 
 	maxraftstate int // snapshot if log grows this big
+	kvmap        map[string]string
+	logI         int
 
 	// Your definitions here.
 }
 
+// helper methods for kv wrapper
+func wrapCKV(c, k, v string) []string {
+	return []string{c, k, v}
+}
+
+func getCKV(wrap interface{}) (c, k, v string) {
+	if wrap, ok := wrap.([]string); ok {
+		c, k, v = wrap[0], wrap[1], wrap[2]
+		return
+	}
+	panic("wrong type")
+}
+
+func (kv *RaftKV) commendIndexKey(cmd string) string {
+	return cmd + "~" + strconv.Itoa(kv.logI)
+}
+
+func (kv *RaftKV) disposeAplMsg() {
+	var lastCKey string
+	for { //todo
+		select {
+		case aplMsg := <-kv.applyCh:
+			wrap := aplMsg.Command.([]string)
+			c, k, v := getCKV(wrap)
+			DAplRecvPrintf("[APLRecv] got me:%d cKey: %s k: %s v: %s\n", kv.me, c, k, v)
+			if strings.HasPrefix(c, "Get") {
+				v = kv.kvmap[k]
+			} else if strings.HasPrefix(c, "Put") {
+				kv.kvmap[k] = v
+			} else {
+				kv.kvmap[k] += v
+			}
+			if c == lastCKey {
+				go func(v string, ch chan string) {
+					ch <- v
+				}(v, kv.syncCh)
+			}
+		case cKey := <-kv.syncCh: // todo assuming the ckey goes ahead from the aplMsg, apparently
+			DAplRecvPrintf("[APLRecv] want %s\n", cKey)
+			lastCKey = cKey
+		}
+	}
+}
+
 func (kv *RaftKV) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	key := args.Key
+	cKey := kv.commendIndexKey("Get")
+	wrap := wrapCKV(cKey, key, "")
+	reply.ServId = kv.me
+	DServPrintf("Get [Request] serv %d wrap %v\n", kv.me, wrap)
+	ind, term, isL := kv.rf.Start(wrap)
+	//todo
+	DServPrintf("Get [Reply] serv %d ind %d term %d isL %t\n", kv.me, ind, term, isL)
+	if !isL {
+		reply.WrongLeader = true
+		return
+	}
+
+	//send the cKey
+	kv.syncCh <- cKey
+
+	//wait the answer or timeout
+	select {
+	case <-time.After(time.Second * 1): // todo, time out 1s ?
+		reply.Err = Error_TimeOut
+	case val := <-kv.syncCh:
+		reply.Value = val
+		kv.logI++
+	}
 }
 
 func (kv *RaftKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	key := args.Key
+	val := args.Value
+	cKey := kv.commendIndexKey(args.Op)
+	wrap := wrapCKV(cKey, key, val)
+	reply.ServId = kv.me
+
+	DServPrintf("PutAppend [Request] serv %d wrap %v\n", kv.me, wrap)
+
+	ind, term, isL := kv.rf.Start(wrap)
+
+	DServPrintf("PutAppend [Reply] serv %d ind %d term %d isL %t\n", kv.me, ind, term, isL)
+	if !isL {
+		reply.WrongLeader = true
+		return
+	}
+
+	//send the cKey
+	kv.syncCh <- cKey
+
+	//wait the answer or timeout
+	select {
+	case <-time.After(time.Second * 4): // todo, time out 1s ?
+		reply.Err = Error_TimeOut
+	case <-kv.syncCh:
+		kv.logI++
+	}
 }
 
 //
@@ -41,6 +143,14 @@ func (kv *RaftKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 func (kv *RaftKV) Kill() {
 	kv.rf.Kill()
 	// Your code here, if desired.
+	DPrintf("Kill Serv %s", kv)
+}
+
+//todo
+func (kv *RaftKV) String() string {
+	buf := bytes.Buffer{}
+	buf.WriteString(fmt.Sprintf("me %d ", kv.me))
+	return buf.String()
 }
 
 //
@@ -67,8 +177,11 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 
 	// Your initialization code here.
 
+	kv.kvmap = make(map[string]string, 1024)
+	kv.syncCh = make(chan string)
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
+	go kv.disposeAplMsg()
 	return kv
 }
